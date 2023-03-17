f37227bbd55f737501bf4dfdc43676b6fe20c775
Doug Schultz <douglasschultz1@gmail.com>
2023-03-17 13:45:15 -0400

    some ch 3 work

--- interpreter/repl/repl.go
+++ interpreter/repl/repl.go
@@ -2,7 +2,9 @@ package repl
 
 import (
 	"bufio"
+	"fish/evaluator"
 	"fish/lexer"
+	"fish/object"
 	"fish/parser"
 	"fmt"
 	"io"
@@ -12,6 +14,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -30,8 +33,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
